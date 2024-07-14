5d546e69a75d3c13cc3d8d8db3ccc7010afd5acb
marcel <marcel.phippen@proton.me>
2024-07-14 23:14:08 +0200

    cleanup settings

--- settings.py
+++ settings.py
@@ -1,27 +1,29 @@
 
-
+# screen
+WIDTH = 1024
+HEIGHT = 512
 FPS = 60
-COUNTDOWN = 1     # number of second before the gameplay start
 
+COUNTDOWN = 3 - 1 # number of second before the gameplay start (starting form 0 so - 1)
+
+# BG
 BACKGROUND_COLOR = ('#000000')  # to replace with assets
 PAUSE_COLOR = ('#ffff00')
-
-MAX_BOUNCE_ANGLE = 60
-
 MAINMENU_BG_COLOR = '#00ffff'
+
 
 # default font. There also is a bold and a mono variant.
 FONT_NAME = 'font/PixeloidSans.ttf'
 FONT_COLOR = '#000000'
 FONT_SIZE = 30
 
-# screen
-WIDTH = 1024
-HEIGHT = 512
 
+# entities
 POWERUP_SPEED = 2
 
-BALL_SPEED = 4
+MAX_BOUNCE_ANGLE = 60
+
+BALL_SPEED = 5
 BALL_COLOR = '#ff0000'
 BALL_RADIUS = 8
 
